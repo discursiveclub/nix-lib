85fd77fe292fb38b5a679ea1c276578b621cf199
sophist <sophist@discursive.club>
2022-08-09 06:34:18 -0400

    Update nix config

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,12 +1,17 @@
 { pkgs, ... }: {
   nix = {
-    autoOptimizeStore = true;
     extraOptions = "experimental-features = nix-command flakes";
     gc = {
       automatic = true;
+      dates = "03:15";
       options = "--delete-older-than 30d";
     };
+    optimise = {
+      automatic = true;
+      dates = [ "04:15" ];
+    };
     package = pkgs.nixFlakes;
+    settings.auto-optimise-store = true;
   };
   nixpkgs.config.allowUnfree = true;
 }
