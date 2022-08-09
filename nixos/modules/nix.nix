545c3ff5306027f422afb7261f813f462ab28695
sophist <sophist@discursive.club>
2022-08-09 06:15:57 -0400

    Configure nix flakes

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,10 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
   nix = {
     autoOptimizeStore = true;
+    extraOptions = "experimental-features = nix-command flakes";
     gc = {
       automatic = true;
       options = "--delete-older-than 30d";
     };
+    package = pkgs.nixFlakes;
   };
   nixpkgs.config.allowUnfree = true;
 };
