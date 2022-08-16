4b64e07ec9d2db7353d1e2d7cec9ebf069f0e4c2
sophist <sophist@discursive.club>
2022-08-16 08:41:04 -0400

    instead of taking arguments for ssmtp, provide defaults

--- nixos/modules/ssmtp.nix
+++ nixos/modules/ssmtp.nix
@@ -1,14 +1,9 @@
-{ authUser, domain, enable, root }:
-{ ... } : {
-  services.ssmtp = {
-    authUser = authUser;
-    authPassFile = "/var/lib/ssmtp/password";
-    domain = domain;
-    enable = enable;
-    hostName = "email-smtp.us-east-1.amazonaws.com:587";
-    root = root;
-    useTLS = true;
-    useSTARTTLS = true;
+{ lib, ... }: {
+  services.ssmtp = with lib; {
+    authPassFile = mkDefault "/var/lib/ssmtp/password";
+    hostName = mkDefault "email-smtp.us-east-1.amazonaws.com:587";
+    useTLS = mkDefault true;
+    useSTARTTLS = mkDefault true;
   };
   users.groups.ssmtp = {};
 }
